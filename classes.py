be3dd7445f34a6cd0e8f0f0c4176d5c7087f88a7
alexhollenbeck <alexhollenbeck@gmail.com>
2015-02-19 12:57:27 -0600

    New class structure

--- classes.py
+++ classes.py
@@ -4,13 +4,6 @@ class tweet:
         self.score = 1
         self.tweetId = 0
 
-class author:
-    def __init__(self):
-        self.score = 1
-        self.tweets = []
-        self.userName = ''
-        self.userId = 0
-
 class relation:
     def __init__(self):
         self.name = ''
@@ -18,4 +11,11 @@ class relation:
         self.reporters = []     
         self.tags = {}          
         self.tagwords = {}      
-        self.words_dict = {}    
+        self.words_dict = {}   
+
+class author:
+    def __init__(self):
+        self.score = 1
+        self.tweets = []
+        self.userName = ''
+        self.userId = 0
